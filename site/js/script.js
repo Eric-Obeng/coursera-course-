f2970a547cb35aeb27a2f543a09e5ea6a4a89a75
Eric Obeng <obengeric.3958@gmail.com>
2022-07-26 08:36:48 -0700

    modify my site directory

---
+++ site/js/script.js
@@ -0,0 +1,26 @@
+/*
+console.log('Enter a value :');
+
+function test(myCondition) {
+    if (myCondition > 10 || myCondition < 20) {
+
+        return "I Love You";
+    }
+    return "I Hate You";
+}
+test(15);
+*/
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1){
+    console.log(names[0]);
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
